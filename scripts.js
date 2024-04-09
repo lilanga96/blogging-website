f6cd36008e11dfb8a69a87303fbc89a497e517c1
Siphesihle Dyalvane <115469219+lilanga96@users.noreply.github.com>
2024-04-09 11:14:55 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -108,7 +108,7 @@ function filterBlogs(category) {
           <img src="${blogImage}" alt="Blog Image">
           <div class="blog-content">
             ${blogDescription}
-            <!-- Add full content of the blog here -->
+      
           </div>
         </div>
       `;
